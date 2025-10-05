69da0ffad72f6c47f766be54f344164c7e76f7e4
K.Dhanuvarshini <127010035@sastra.ac.in>
2025-10-05 20:18:32 +0530

    Add files via upload

---
+++ example8.py
@@ -0,0 +1,16 @@
+import cv2
+alg="haarcascade_frontalface_default.xml"
+haarcascade=cv2.CascadeClassifier(alg)#Loading algorithm(for every type of loading there will be algorithm)
+cam=cv2.VideoCapture(0)#Camera ID Initialization
+while True:
+    _,img=cam.read()#Reading the frame camera
+    grayImg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)#Converting to gray scale image
+    face=haarcascade.detectMultiScale(grayImg,1.3,4)#scaling factor and minimal facing pattern(passing them)
+    for(x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.imshow("FaceDetection",img)
+        key = cv2.waitKey(10)
+    if key==27:
+        break
+cam.release()
+cv2.destroyAllWindows()
